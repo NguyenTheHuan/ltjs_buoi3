694b3686ca14a46a0e1ee0dc100553e407b567d9
NguyenTheHuan <nguyenthehuan@students.hueic.edu.vn>
2025-10-01 20:18:37 +0700

    chinh sua

--- bai5.js
+++ bai5.js
@@ -1,3 +1,3 @@
 let year = prompt("Nhap nam can kiem tra:");
 let result = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? "Nam nhuan" : "Nam khong nhuan";
-console.log(result);
+document.write("Nam " + year + " la: " + result);
