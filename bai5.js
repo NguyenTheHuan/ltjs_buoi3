e00ae467302856ceb3ce4bf19a5b4b4f6d0acafd
NguyenTheHuan <nguyenthehuan@students.hueic.edu.vn>
2025-10-01 20:42:39 +0700

    nop bai

--- bai5.js
+++ bai5.js
@@ -1,3 +1,3 @@
 let year = prompt("Nhap nam can kiem tra:");
-let result =(year <= 0) ? " Nam khong hop le" : (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? "Nam nhuan" : "Nam khong nhuan";
+let result =(year <= 0 || isNaN(year)) ? " Nam khong hop le" : (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? "Nam nhuan" : "Nam khong nhuan";
 document.write("Nam " + year + " la: " + result);
